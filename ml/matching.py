a8805b40f90be55e1c9b1c9f6d2496179e8a1815
Vitaliy Prachov <betalik1997@gmail.com>
2022-10-08 02:10:19 +0300

    feat: добавил расчет центральных новостей в группе

--- ml/matching.py
+++ ml/matching.py
@@ -1,3 +1,6 @@
+from numpy import ndarray
+from sklearn.neighbors import NearestNeighbors
+
 from config import BATCH_SIZE, USE_POOLING
 
 from typing import List
@@ -8,3 +11,12 @@ def match_news(news: List[str], vectorizer, clusterer) -> List[int]:
     groups = clusterer.fit_predict(embeddings)
 
     return groups
+
+
+def get_indexes_central_components(embeddings: ndarray, centroids: ndarray) -> ndarray:
+    neigh = NearestNeighbors(n_neighbors=1)
+    neigh.fit(embeddings)
+
+    indexes = neigh.kneighbors(centroids, return_distance=False)[:, 0]
+
+    return indexes
