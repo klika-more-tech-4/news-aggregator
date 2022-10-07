e287b4a4aa1a7178d77e76bc93728b05edada52b
umutaev <M8858110177@yandex.com>
2022-10-07 21:05:51 +0300

    feat: парсер банкиру

--- parsers/bankiru/config.py
+++ parsers/bankiru/config.py
@@ -1,8 +1,15 @@
 from typing import Union
 from datetime import datetime, date
 
-BASE_URL = "https://www.banki.ru/news/lenta/business/"
+BASE_URL = "https://www.banki.ru/news/lenta/"
+SOURCE = "BANKIRU"
+
 
 def generate_url_date(d: Union[datetime, date]) -> str:
-    additional_url = f"?filterType=all&d={d.day}&m={d.month}&y={d.year}"
+    additional_url = f"business/?filterType=all&d={d.day}&m={d.month}&y={d.year}"
+    return BASE_URL + additional_url
+
+
+def generate_news_url(id: str) -> str:
+    additional_url = f"?id={id}"
     return BASE_URL + additional_url
--- parsers/bankiru/parser.py
+++ parsers/bankiru/parser.py
@@ -5,7 +5,8 @@ import re
 import requests
 from bs4 import BeautifulSoup, SoupStrainer
 
-from parsers.bankiru.config import generate_url_date
+from parsers.bankiru.config import generate_url_date, generate_news_url, SOURCE
+from entities.news import NewsDTO
 
 
 def parse_news_for_date(d: datetime) -> List[str]:
@@ -19,3 +20,31 @@ def parse_news_for_date(d: datetime) -> List[str]:
             except:
                 pass
     return list(set(ids))
+
+
+def parse_single_news(id: str) -> NewsDTO:
+    link = generate_news_url(id)
+    r = requests.get(link)
+    bs4 = BeautifulSoup(r.text, features="html.parser")
+    title = bs4.find_all("h1", {"class": "text-header-0"})[0].text
+    creation_date = datetime.strptime(
+        (
+            bs4.find_all("div", {"style": "padding-top: 24px;"})[0]
+            .findChildren("div", recursive=False)[0]  # Тулбар
+            .findChildren("div", recursive=False)[1]  # Правая часть с датой и просмотрами
+            .findChildren("span")[0]  # Дата
+            .text.strip()
+        ),
+        "%d.%m.%Y %H:%M",
+    )
+    links = []
+    text = []
+    for paragraph in bs4.find_all("div", {"itemprop": "articleBody"})[0].findChildren("p", recursive=False):
+        for a_link in paragraph.find_all("a"):
+            if "mailto:" not in a_link["href"]:
+                links.append(a_link["href"])
+        text.append(paragraph.text)
+
+    return NewsDTO(
+        link=link, text="\n".join(text), title=title, source=SOURCE, timestamp=creation_date, refers_to=links
+    )
