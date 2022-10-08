8f30e0e4974ca8cb00d64b0b2d80af85f101d350
Vitaliy Prachov <betalik1997@gmail.com>
2022-10-08 09:13:54 +0300

    feat: изменил формат выхода функции матчинга новостей

--- ml/matching.py
+++ ml/matching.py
@@ -3,12 +3,17 @@ from sklearn.neighbors import NearestNeighbors
 
 from config import BATCH_SIZE, USE_POOLING
 
-from typing import List
+from typing import List, DefaultDict
+from collections import defaultdict
 
 
-def match_news(news: List[str], vectorizer, clusterer) -> List[int]:
+def match_news(news: List[str], vectorizer, clusterer) -> DefaultDict[int, List[str]]:
     embeddings = vectorizer.vectorize(news, batch_size=BATCH_SIZE, use_pooling=USE_POOLING)
-    groups = clusterer.fit_predict(embeddings)
+    labels = clusterer.fit_predict(embeddings)
+
+    groups = defaultdict(list)
+    for label, title in zip(labels, news):
+        groups[label].append(title)
 
     return groups
 
